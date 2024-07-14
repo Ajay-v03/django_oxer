e911a1563e264b57ce9ebca0a998949a309633a3
Ajay Verma <ajay7monu@gmail.com>
2024-07-14 13:44:23 +0530

    POST method implemented in Form

--- oxer/oxer/views.py
+++ oxer/oxer/views.py
@@ -13,14 +13,21 @@ def blog(request):
 def class_type(request):
     return render(request,"class.html")
 
-# GET Method example
+# GET/POST Method example
 def form(request):
-    full_name = ''
+    data = {}
     try:
-        first_name = request.GET['fname']
-        last_name = request.GET['lname']
+        # first_name = request.GET['fname']
+        # last_name = request.GET['lname']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
         full_name = first_name + ' ' + last_name
+        data = {
+            'fname':first_name,
+            'lname':last_name,
+            'fullname':full_name
+        }
     except:
         pass
-    print(full_name)
-    return render(request,"form.html",{'output':full_name})
+    print(data)
+    return render(request,"form.html",data)
